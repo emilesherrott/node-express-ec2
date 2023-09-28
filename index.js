ae316cc67151a0758cefa81a6856fa069a4cbebe
“emilesherrott” <emilesherrott@gmail.com>
2023-09-28 13:24:05 +0100

    Updated port

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import app from "./app.js"
 
 import 'dotenv/config'
 
+const port = process.env.PORT || 3000
 
-
-app.listen(process.env.PORT, () => {
-    console.log(`App up and running on port ${process.env.PORT}`)
+app.listen(port, () => {
+    console.log(`App up and running on port ${port}`)
 })
