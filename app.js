3ea32ed40a77eb062be61011ca8d5c6d4fa82772
“emilesherrott” <emilesherrott@gmail.com>
2023-09-28 13:49:26 +0100

    Updated port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-    res.json("Hello World")
+    res.json({story: `The story goes that a race was organised by the Jade Emperor - one of the most important gods in traditional Chinese religion - who invited all the animals in the world to take part. Twelve species turned up at the start line: a pig, dog, rooster, monkey, sheep, horse, snake, dragon, rabbit, tiger, ox and rat. As a reward for turning up, the Emperor named a year in the zodiac after each one, while the race would determine the order each animal would be placed.`})
 })
 
 app.get('/:year', (req, res) => {
